20b6b06a9352be52e8643dc85433def46e91eaff
Aidan <olabaz@gmail.com>
2020-07-30 18:04:33 -0400

    Change script to add spacing to header file and definition of NUM_LANGS

--- tools/gen_lang_list.sh
+++ tools/gen_lang_list.sh
@@ -1,3 +1,3 @@
-printf "#ifndef LANG_LIST_H\n#define LANG_LIST_H\n#define NUM_LANGS %s\n" `ls ../external/templates/templates/*.gitignore | wc -l` > ../src/lang_list.h
+printf "#ifndef LANG_LIST_H\n#define LANG_LIST_H\n\n#define NUM_LANGS %s\n\n" `ls ../external/templates/templates/*.gitignore | wc -l` > ../src/lang_list.h
 ls ../external/templates/templates/*.gitignore | cut -f 5 -d '/' | cut -f 1 -d '.' | sed -e "s/\(.*\)/    \"\1\",/" | sed -e "\$s/,/\n};/" | sed -e "1s/    \"/char* lang_list[]={\n    \"/" | sed -e "s/[A-Z]/\L&/g" >> ../src/lang_list.h
 printf "\n#endif" >> ../src/lang_list.h
